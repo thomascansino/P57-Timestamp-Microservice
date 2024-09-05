2b894a78d615dced2ef6c9ea1ce43bd62b35c6e5
thomascansino <thomas.cansino@gmail.com>
2024-09-05 22:11:04 +0800

    feat: added handler functions to specified routes to complete the project

--- index.js
+++ index.js
@@ -5,6 +5,22 @@
 var express = require('express');
 var app = express();
 
+const isValidUnixTimestamp = (input) => {
+  const timestamp = Number(input);
+  const date = new Date(timestamp);
+  return !isNaN(timestamp) && !isNaN(date.getTime());
+};
+
+const isValidDateString = (input) => {
+  const datePattern = /^\d{4}-\d{2}-\d{2}$/;
+  if ( !datePattern.test(input) ) {
+    return false;
+  }
+
+  const date = new Date(input);
+  return !isNaN(date.getTime()) && input === date.toISOString().split('T')[0];
+};
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
@@ -24,7 +40,45 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:date?", (req, res) => {
+  
+  const input = req.params.date;
 
+  if ( isValidDateString(input) ) {
+    const date = new Date(input);
+    const unix = date.getTime();
+    const utc = date.toUTCString();
+
+    res.json({
+      "unix": unix,
+      "utc": utc
+    })
+  } else if ( isValidUnixTimestamp(input) ) {
+    const timestamp = Number(input);
+    const date = new Date(timestamp);
+    const unix = date.getTime();
+    const utc = date.toUTCString();
+
+    res.json({
+      "unix": unix,
+      "utc": utc
+    })
+  } else if ( !input ) {
+    const date = new Date();
+    const unix = date.getTime();
+    const utc = date.toUTCString();
+
+    res.json({
+      "unix": unix,
+      "utc": utc
+    })
+  } else {
+    res.json({
+      "error": "Invalid Date"
+    })
+  }
+  
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
